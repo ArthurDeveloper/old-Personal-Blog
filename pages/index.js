bf9cb6287b11a5f462f32fa573e2ba92b8ca1a03
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-06-11 22:24:24 -0300

    Aprimoramento na interface inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
 import Page from '../src/components/pageComponents/Page.jsx'
 import TopBar from '../src/components/pageComponents/TopBar.jsx'
 import Item from '../src/components/pageComponents/Item.jsx'
@@ -11,19 +10,24 @@ import Link from '../src/components/pageComponents/Link.jsx'
 
 import styles from '../src/styles/home.module.scss'
 
-
 export default function App() {
+
   return (
 
     <Page>
 
-      <h1 className="title">
-        Blog do Arthur Dev &#x1F601;
-      </h1>
+      <div id={styles['profile']}>
+        <img id={styles['profile-img']} className="circle-radius" src="https://github.com/arthurdeveloper.png" />
+        <h1 className="title centered" style={{marginBottom: "20px"}}>
+          Arthur Dev
+        </h1>
 
-      <h2 className="subtitle">
-        Aqui você encontra diversos assuntos sobre programação e tecnologia em geral!
-      </h2>
+        <h2 className="subtitle centered">
+          Desenvolvedor web e games
+        </h2>
+      </div>
+
+      
 
     </Page> 
 
